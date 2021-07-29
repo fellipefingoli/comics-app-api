4d4c53cc24550ffac8681a96c852de6ff6db0c2d
Fellipe Fingoli <fellipe.fingoli@gmail.com>
2021-07-29 18:47:00 -0300

    Fix Users migration, clean the Gemfile and install rspec

--- db/migrate/20210729212219_create_users.rb
+++ db/migrate/20210729212219_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :email, index:true, null: false
-      t.string :password. null: false
+      t.string :password, null: false
 
       t.timestamps
     end
